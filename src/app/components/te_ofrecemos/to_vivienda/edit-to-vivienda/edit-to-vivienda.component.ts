e7f4e49b84f31044e0a98811bf5f759d20a12917
julietaArgollas <julieta.cargollas@gmail.com>
2020-04-28 23:48:14 -0400

    teOfrecemosVivienda

--- src/app/components/te_ofrecemos/to_vivienda/edit-to-vivienda/edit-to-vivienda.component.ts
+++ src/app/components/te_ofrecemos/to_vivienda/edit-to-vivienda/edit-to-vivienda.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialogRef}from '@angular/material/dialog';
+import { TeofrecemosService } from 'src/app/services/teofrecemos.service';
+import {NgForm}from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSnackBar}from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort}from '@angular/material/sort';
+import {ViewChild}from '@angular/core';
 
 @Component({
   selector: 'app-edit-to-vivienda',
@@ -7,9 +17,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditToViviendaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackBar:MatSnackBar, private router: Router,public dialogbox:MatDialogRef<EditToViviendaComponent>,public service:TeofrecemosService) { 
+    this.service.listen().subscribe((m:any)=>{
+      console.log(m);
+      this.charge();
+    });
+  }
+
+  listData:MatTableDataSource<any>;
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit(): void {
+  }
+
+  charge(){
+    this.service.getTeOfrecemosVivienda().subscribe(data=>{
+      this.listData= new MatTableDataSource(data);
+      this.listData.sort=this.sort;
+    });
+  }
+
+  close(){
+    this.dialogbox.close();
+  }
+  onSubmit(form:NgForm){
+    this.service.updateTeOfrecemos(form.value).subscribe(res=>
+    {
+      this.dialogbox.close();
+      this.service.filter("Register click");
+      this.snackBar.open('Editado con exito','',{
+        duration:5000,
+        verticalPosition:'top'
+      })
+    })
+
   }
 
 }
--- src/app/components/te_ofrecemos/to_vivienda/show-to-vivienda/show-to-vivienda.component.ts
+++ src/app/components/te_ofrecemos/to_vivienda/show-to-vivienda/show-to-vivienda.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild} from '@angular/core';
+import { Router } from '@angular/router';
+import { TeofrecemosService } from 'src/app/services/teofrecemos.service';
+import { Subscriber} from 'rxjs';
+import { TeOfrecemos } from 'src/app/models/TeOfrecemos';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort}from '@angular/material/sort';
+import{MatDialog,MatDialogConfig}from '@angular/material/dialog';
+import { filter } from 'rxjs/operators';
+import{Subject}from 'rxjs';
+import {Observable} from 'rxjs';
+import {MatSnackBar}from '@angular/material/snack-bar';
+import { EditViviendaComponent } from 'src/app/components/referente/vivienda/edit-vivienda/edit-vivienda.component';
+import { AddViviendaComponent } from 'src/app/components/referente/vivienda/add-vivienda/add-vivienda.component';
+
 
 @Component({
   selector: 'app-show-to-vivienda',
@@ -7,9 +21,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowToViviendaComponent implements OnInit {
 
-  constructor() { }
+  teofrecemos:TeOfrecemos[];
 
-  ngOnInit(): void {
+  dataSource=null;
+
+  constructor(private snackBar:MatSnackBar,private teofrecemosService: TeofrecemosService, private router: Router,private dialog: MatDialog) { 
+    this.teofrecemosService.listen().subscribe((m:any)=>{
+      console.log(m);
+      this.charge();
+    });
+  }
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  ngOnInit() {
+    this.teofrecemosService.getTeOfrecemosVivienda().subscribe(data =>{
+      this.teofrecemos = data;
+      this.charge();
+    });
+  }
+
+  charge(){
+    this.teofrecemosService.getTeOfrecemosVivienda().subscribe(data=>{
+      this.teofrecemos=data;
+    });
+  }
+
+  edit_Vivienda(teOfrecemos: TeOfrecemos){
+    this.teofrecemosService.formData=teOfrecemos;
+    const dialogConfig=new MatDialogConfig();
+   dialogConfig.disableClose=true;
+   dialogConfig.autoFocus=true;
+   dialogConfig.width="70%";
+   this.dialog.open(EditViviendaComponent,dialogConfig);
+
+   localStorage.setItem("id_teOfrecemos", teOfrecemos.id_teOfrecemos .toString());
+  }
+
+  Delete(teOfrecemos: TeOfrecemos){
+    if(confirm('Estas seguro de eliminar ? ')){
+      this.teofrecemosService.deleteHVT(teOfrecemos)
+      .subscribe(data=>{
+        //.historiasHVT=this.historiasHVT.filter(p=>p.id_HVT!==historiaVidaTexto.id_HVT);
+        this.charge();
+        this.snackBar.open('Eliminado Correctamente','',{
+          duration:5000,
+          verticalPosition:'top'
+        });
+     });
+    }
+   }
+
+  AddVivienda(){
+    const dialogConfig=new MatDialogConfig();
+    dialogConfig.disableClose=true;
+    dialogConfig.autoFocus=true;
+    dialogConfig.width="70%";
+    this.dialog.open(AddViviendaComponent,dialogConfig);
   }
 
 }
