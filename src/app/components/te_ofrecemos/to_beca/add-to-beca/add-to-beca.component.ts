a4c5cbbb21f9a64506ab86c36b424b1b13c1346b
Alvaro Flores Farfan <alvaro.fafy98@gmail.com>
2020-04-28 21:27:52 -0400

    beca

--- src/app/components/te_ofrecemos/to_beca/add-to-beca/add-to-beca.component.ts
+++ src/app/components/te_ofrecemos/to_beca/add-to-beca/add-to-beca.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialogRef }from '@angular/material/dialog';
+import { TeofrecemosService } from 'src/app/services/teofrecemos.service';
+import {NgForm}from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSnackBar}from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort}from '@angular/material/sort';
+import {ViewChild}from '@angular/core';
 @Component({
   selector: 'app-add-to-beca',
   templateUrl: './add-to-beca.component.html',
@@ -7,9 +16,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddToBecaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackBar:MatSnackBar, private router: Router,public dialogbox: MatDialogRef<AddToBecaComponent>, public service: TeofrecemosService) { 
+    this.service.listen().subscribe((m:any)=>{
+      console.log(m);
+      this.charge();
 
-  ngOnInit(): void {
+    });
   }
 
+  listData:MatTableDataSource<any>; 
+
+  ngOnInit(): void {
+    this.resetForm();
+  }
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  close(){
+    this.dialogbox.close();
+  }
+
+  resetForm(form?:NgForm){
+    if(form!=null)
+    form.resetForm();
+    this.service.formData={
+      id_teOfrecemos: 0,
+      tema: '',
+      contenido: '',
+      contacto: ''
+    }
+  }
+  onSubmit(form:NgForm){
+    this.service.createTeOfrecemosBeca(form.value).subscribe(res=>{
+      this.resetForm(form);
+      this.dialogbox.close();
+    this.service.filter("Register click");
+      this.snackBar.open('Añadido correctamente','',{
+        duration:5000,
+        verticalPosition:'top'
+      });
+    })
+  }
+
+  charge(){
+    this.service.getTeOfrecemosBeca().subscribe(data=>{
+      this.listData= new MatTableDataSource(data);
+      this.listData.sort=this.sort;
+    });
+  }
+
+  Guardar() {
+    this.service.createTeOfrecemosBeca(this.service.formData).subscribe(data => {
+      this.snackBar.open('Añadido correctamente','',{
+        duration:5000,
+        verticalPosition:'top'
+      });
+    })
+    this.close();
+  }
 }
--- src/app/components/te_ofrecemos/to_beca/edit-to-beca/edit-to-beca.component.ts
+++ src/app/components/te_ofrecemos/to_beca/edit-to-beca/edit-to-beca.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MatDialogRef}from '@angular/material/dialog';
+import { TeofrecemosService } from 'src/app/services/teofrecemos.service';
+import {NgForm}from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSnackBar}from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort}from '@angular/material/sort';
+import {ViewChild}from '@angular/core';
 @Component({
   selector: 'app-edit-to-beca',
   templateUrl: './edit-to-beca.component.html',
@@ -7,9 +16,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditToBecaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackBar:MatSnackBar, private router: Router,public dialogbox:MatDialogRef<EditToBecaComponent>,public service:TeofrecemosService) { 
+    this.service.listen().subscribe((m:any)=>{
+      console.log(m);
+      this.charge();
+    });
+  }
+  listData:MatTableDataSource<any>;
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit(): void {
   }
 
+  charge(){
+    this.service.getTeOfrecemosBeca().subscribe(data=>{
+      this.listData= new MatTableDataSource(data);
+      this.listData.sort=this.sort;
+    });
+  }
+
+  close(){
+    this.dialogbox.close();
+  }
+  onSubmit(form:NgForm){
+    this.service.updateTeOfrecemos(form.value).subscribe(res=>
+    {
+      this.dialogbox.close();
+      this.service.filter("Register click");
+      this.snackBar.open('Editado con exito','',{
+        duration:5000,
+        verticalPosition:'top'
+      })
+    })
+
+  }
 }
