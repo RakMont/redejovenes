01e106bd64f5a931cd95dec9b05c3edc2ca380a5
Raquel Montero <nabogaria@gmail.com>
2020-06-02 04:07:41 -0400

    shows moderator

--- src/app/components/admin/showmoderator/showmoderator.component.ts
+++ src/app/components/admin/showmoderator/showmoderator.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
+import{MatDialog,MatDialogConfig}from '@angular/material/dialog';
+
+import { filter } from 'rxjs/operators';
+import{Subject}from 'rxjs';
+import {Observable} from 'rxjs';
+import {MatSnackBar}from '@angular/material/snack-bar';
+
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { Usuario } from 'src/app/models/Usuario';
 @Component({
   selector: 'app-showmoderator',
   templateUrl: './showmoderator.component.html',
   styleUrls: ['./showmoderator.component.css']
 })
 export class ShowmoderatorComponent implements OnInit {
-
-  constructor() { }
+  usuarios:Usuario[];
+  user:Usuario;
+  constructor(private tokenStorageService: TokenStorageService,private snackBar:MatSnackBar,private service: UserService, private router: Router,private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.service.getAllModerators()
+   .subscribe(data =>{
+     this.usuarios = data;
+    this.user=this.usuarios[0];
+    console.log(this.user.roles[0]);
+   });
+  }
+  Delete(user: Usuario){
+    if(confirm('Estas seguro de eliminar ? ')){
+      this.service.deleteHVT(user)
+      .subscribe(data=>{
+        this.usuarios=this.usuarios.filter(p=>p==user);
+        this.snackBar.open('Eliminado Correctamente','',{
+          duration:5000,
+          verticalPosition:'top'
+        });
+     });
+    }
+
   }
 
 }
