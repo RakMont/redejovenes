4f191324efd2e9db4e609147b28cecbefcd266ed
ale <ale7cosch@gmail.com>
2020-06-03 03:51:46 -0400

    Quienes somos done

--- src/app/components/quienes-somos/quienes-somos.component.ts
+++ src/app/components/quienes-somos/quienes-somos.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quienes-somos.component.css']
 })
 export class QuienesSomosComponent implements OnInit {
-
+  images=[];
   constructor() { }
 
   ngOnInit(): void {
+    this.images[1]="../../assets/img-1.jpg"
+    this.images[2]="../../assets/img-2.jpg"
+    this.images[3]="../../assets/img-3.jpg"
   }
 
 }
