ec3944e0fd4c453b96704d6e5dc0002c3c13b43c
Raquel Montero <nabogaria@gmail.com>
2020-06-09 01:51:56 -0400

    edit trabajo comentary

--- src/app/components/comentario/trabajo/edit-comentario-trabajo/edit-comentario-trabajo.component.ts
+++ src/app/components/comentario/trabajo/edit-comentario-trabajo/edit-comentario-trabajo.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import{MatDialogRef}from '@angular/material/dialog';
+import{ComentarioService}from 'src/app/services/comentario.service';
+import{NgForm}from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSnackBar}from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatSort}from '@angular/material/sort';
+import {ViewChild}from '@angular/core';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { Usuario } from 'src/app/models/Usuario';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-edit-comentario-trabajo',
@@ -6,10 +19,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-comentario-trabajo.component.css']
 })
 export class EditComentarioTrabajoComponent implements OnInit {
+  private roles: string[];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username: string;
+  id;
+  userprof:Usuario;
 
-  constructor() { }
-
+  constructor(private userservice:UserService,private tokenStorageService: TokenStorageService,private snackBar:MatSnackBar, private router: Router,public dialogbox:MatDialogRef<EditComentarioTrabajoComponent>,public service:ComentarioService) { }
   ngOnInit(): void {
-  }
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
 
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.id=user.id;
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+    this.userservice.getUserProfile(this.username)
+    .subscribe(data =>{
+    this.userprof = data;
+    console.log(this.userprof);
+  });
+  }
+close(){
+this.dialogbox.close();
+}
+
+onSubmit(){
+  //this.Guardar();
+  this.service.formData.user=this.userprof;
+  console.log(this.service.formData);
+
+  this.service.editarComentarioTrabajo(this.service.formData).subscribe(res=>{
+    this.dialogbox.close();
+  this.service.filter("Register click");
+    this.snackBar.open('Editado  correctamente','',{
+      duration:5000,
+      verticalPosition:'top'
+    });
+  })
+
+  window.location.reload();
+
+}
 }
